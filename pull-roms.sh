9360642a0f0f7ffc698f2398658fa8848912c028
claudiol <claudiol@users.sourceforge.net>
2021-11-21 16:41:31 -0500

    Fixed URLs.

--- pull-roms.sh
+++ pull-roms.sh
@@ -5,8 +5,8 @@ FWDIR=$(pwd)/firmware
 echo Retrieving Bootloader from hpcalc.org for HP 49g+ and HP 50g...
 mkdir -p $TEMPDIR
 pushd $TEMPDIR
-wget https://www.hpcalc.org/hp49/pc/rom/HP-ARM-Models-Bootloader.zip
-unzip -j HP-ARM-Models-Bootloader.zip -d $TEMPDIR
+wget https://www.hpcalc.org/hp49/pc/rom/hp-arm-models-bootloader.zip
+unzip -j hp-arm-models-bootloader.zip -d $TEMPDIR
 cp *.bin $FWDIR
 popd
 rm -rf $TEMPDIR
@@ -24,7 +24,7 @@ rm -rf $TEMPDIR
 echo Retrieving latest Unofficial newRPL build...
 mkdir -p $TEMPDIR
 pushd $TEMPDIR
-wget https://www.hpgcc3.org/downloads/newrplfw.bin
+wget https://hpgcc3.org/downloads/newrplfw.bin
 cp newrplfw.bin $FWDIR
 popd
 rm -rf $TEMPDIR
