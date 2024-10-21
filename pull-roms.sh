b12cce480aa11c57c842027af7bb07d8bf3b7bf6
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2024-10-21 17:49:33 +0200

    refactor pull-roms.sh

--- pull-roms.sh
+++ pull-roms.sh
@@ -1,31 +1,24 @@
-TEMPDIR=${TEMPDIR:-$(pwd)/tmpdir}
-FWDIR=${FWDIR:-$(pwd)/firmware}
+CWD=$(pwd)
+TEMPDIR=${TEMPDIR:-$CWD/tmpdir}
+FWDIR=${FWDIR:-$CWD/firmware}
 
-mkdir -p $FWDIR
+mkdir -p "$FWDIR"
+mkdir -p "$TEMPDIR"
+cd "$TEMPDIR" || exit 1
 
 echo Retrieving Bootloader from hpcalc.org for HP 49g+ and HP 50g...
-mkdir -p $TEMPDIR
-pushd $TEMPDIR
 wget https://www.hpcalc.org/hp49/pc/rom/hp-arm-models-bootloader.zip
-unzip -j hp-arm-models-bootloader.zip -d $TEMPDIR
-cp *.bin $FWDIR/
-popd
-rm -rf $TEMPDIR
-
+unzip -j hp-arm-models-bootloader.zip -d "$TEMPDIR"
+mv ./*.bin "$FWDIR"/
 
 echo Retrieving ROM 2.15 from hpcalc.org for HP 49g+ and HP 50g...
-mkdir -p $TEMPDIR
-pushd $TEMPDIR
 wget https://www.hpcalc.org/hp49/pc/rom/hp4950v215.zip
-unzip -j hp4950v215.zip -d $TEMPDIR
-cp *.bin $FWDIR/
-popd
-rm -rf $TEMPDIR
+unzip -j hp4950v215.zip -d "$TEMPDIR"
+mv ./*.bin "$FWDIR"/
 
 echo Retrieving latest Unofficial newRPL build...
-mkdir -p $TEMPDIR
-pushd $TEMPDIR
 wget https://hpgcc3.org/downloads/newrplfw.bin
-cp newrplfw.bin $FWDIR/
-popd
-rm -rf $TEMPDIR
+mv ./newrplfw.bin "$FWDIR"/
+
+cd "$CWD" || exit 1
+rm -rf "$TEMPDIR"
