0eb3bce4374d9c7d676163a7236ed60c95233c70
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2025-04-17 14:50:38 +0200

    delete more dead (debug) code & saturn.h

--- src/x50ng/s3c2410/block-vvfat.c
+++ src/x50ng/s3c2410/block-vvfat.c
@@ -2950,20 +2950,6 @@ static void checkpoint( const char* where )
     DLOG( fprintf( stderr, "checkpoint(%s): call check2()\n", where ) );
     check2( vvv, where );
     assert( !vvv->current_mapping || vvv->current_fd || ( vvv->current_mapping->mode & MODE_DIRECTORY ) );
-#  if 0
-    if (((direntry_t*)vvv->directory.pointer)[1].attributes != 0xf)
-	fprintf(stderr, "Nonono!\n");
-    mapping_t* mapping;
-    direntry_t* direntry;
-    assert(vvv->mapping.size >= vvv->mapping.item_size * vvv->mapping.next);
-    assert(vvv->directory.size >= vvv->directory.item_size * vvv->directory.next);
-    if (vvv->mapping.next<47)
-	return;
-    assert((mapping = array_get(&(vvv->mapping), 47)));
-    assert(mapping->dir_index < vvv->directory.next);
-    direntry = array_get(&(vvv->directory), mapping->dir_index);
-    assert(!memcmp(direntry->name, "USB     H  ", 11) || direntry->name[0]==0);
-#  endif
     DLOG( fprintf( stderr, "checkpoint(%s): done\n", where ) );
     return;
     /* avoid compiler warnings: */
